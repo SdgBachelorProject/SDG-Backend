04a45201b8c28e4822a6f2cf72b1ac974a206817
SprytnyMarek <jtytanowy69@gmail.com>
2022-12-01 15:08:06 +0100

    settings change 2

--- SDG_Backend/settings.py
+++ SDG_Backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4*=db+m!90$&z&m@y2va$3$y)$*e1-972q_+ht0fmcj93t*)bn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['sdgbackendapp.azurewebsites.net']
 
 
 # Application definition
